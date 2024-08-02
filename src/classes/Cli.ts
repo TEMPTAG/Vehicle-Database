9be386c47651bdee1c485e99599ff14fb0560752
Ian Ferguson <iansterlingferguson@gmail.com>
2024-08-02 14:55:27 -0700

    tow action functional on truck only, and returns Only Trucks can tow if other vehicle is selected to tow.

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -371,6 +371,20 @@ class Cli {
             }
           }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        } else if (answers.action === 'Tow') {
+          let truck: Truck | undefined;
+
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin && this.vehicles[i] instanceof Truck) {
+              truck = this.vehicles[i] as Truck;
+            }
+          }
+          if (truck) {
+            this.findVehicleToTow();
+            return;
+          } else {
+            console.log('Only Trucks can tow');
+          }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         } else if (answers.action === 'Wheelie') {
           let motorbike: Motorbike | undefined;
@@ -381,9 +395,9 @@ class Cli {
             }
           }
           if (motorbike) {
-              motorbike.wheelie();
-            } else {
-              console.log('Only Motorbikes can perform a Wheelie');
+            motorbike.wheelie();
+          } else {
+            console.log('Only Motorbikes can perform a Wheelie');
           }
         } else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
